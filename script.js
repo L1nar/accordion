1ec7d5886c05d7d761c136579bb0699c03feda81
L1nar <122251664+L1nar@users.noreply.github.com>
2024-03-15 12:21:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let parent = document.querySelector('#parent');
+let tabs = document.querySelectorAll('.tab');
+let links = document.querySelectorAll('a');
+let img = document.querySelectorAll('.text');
+
+parent.addEventListener('click', (event) => {
+    event.preventDefault();
+    let target = event.target;
+    console.log(target);
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('active');
+        if (target.closest('.img')) {
+            tabs[i].classList.add('active')
+        }
+    }
+})      
